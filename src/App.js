5c7b7ccf5549945d2819d94a98900bc1e44c0f18
Frederico Mottinha de Figueiredo <contato@fredericomf.com>
2019-01-26 19:30:31 -0200

    Lesson 'Estilizando Sidebar'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Fragment, Component } from "react";
 import { GlobalStyle } from "./styles/global";
+import Sidebar from "./components/Sidebar";
 
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <GlobalStyle />
-        <h1>Hello World</h1>
-      </Fragment>
-    );
-  }
-}
+const App = () => (
+  <Fragment>
+    <GlobalStyle />
+    <Sidebar />
+  </Fragment>
+);
 
 export default App;
